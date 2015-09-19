5a3587096d29399c98fa5519b0b3ebf8299c79ae
Rastafire <pascal.libenzi@gmail.com>
2015-09-19 17:08:33 +0200

    upgrade

--- HudsonClientV2/src/hudsonclientv2/bo/JobState.java
+++
@@ -1,9 +0,0 @@
-package hudsonclientv2.bo;
-
-/**
- * @author libenzi
- * Defines states for a job. It could be ok (green or blue), ko (red) or unstable (yellow)
- */
-public enum JobState {
-	OK, KO, UNSTABLE;
-}
