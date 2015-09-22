5ebb964c80f06057a44dd67d6bbb702bcb780032
Rastafire <pascal.libenzi@gmail.com>
2015-09-22 20:36:17 +0200

    Useless thing

--- HudsonClientV2/src/hudsonclientv2/bo/JobState.java
+++ HudsonClientV2/src/hudsonclientv2/bo/JobState.java
@@ -5,5 +5,33 @@ package hudsonclientv2.bo;
  * Defines states for a job. It could be ok (green or blue), ko (red) or unstable (yellow)
  */
 public enum JobState {
-	OK, KO, UNSTABLE;
+	OK(true), 
+	KO(false, true), 
+	UNSTABLE(false);
+	
+	private boolean green = false;
+	private boolean red = false;
+	
+	private JobState(boolean green) {
+		this.green = green;
+    }
+
+	private JobState(boolean green, boolean isRed) {
+		this.green = green;
+		this.red = isRed;
+    }
+
+	public boolean isGreen() {
+	    return green;
+    }
+	
+	public boolean isRed() {
+	    return red;
+    }
+	
+	public static void main(String[] args) {
+		JobBuild jobBuild = new JobBuild(OK, "");
+		System.out.println(jobBuild.getCurrentState().isGreen());
+		System.out.println(jobBuild.getCurrentState().isRed());
+		}
 }
